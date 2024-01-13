a463d7c0c995eefd819ab6aefbed93a77ca431e3
bala1802 <bala1802@live.com>
2024-01-13 15:20:42 +0530

    configured the device as mps

--- prediction.py
+++ prediction.py
@@ -10,12 +10,10 @@ def predict(prompt, pipe, loss_function=None):
     latents = []
     
     for seed_number, sd_concept in zip(config.SEEDS, config.STABLE_DIFUSION_CONCEPTS):
-        # torch.mps.empty_cache()
-        torch.gpu.empty_cache()
+        torch.mps.empty_cache()
         gc.collect()
-        # torch.mps.empty_cache()
-        torch.gpu.empty_cache()
-
+        torch.mps.empty_cache()
+        
         prompt = [f'{prompt} {sd_concept}']
         latent = generator.generate_images(pipe=pipe, seed_number=seed_number, prompt=prompt, loss_function=loss_function)
         latents.append(latent)
